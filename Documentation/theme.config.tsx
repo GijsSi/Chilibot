cbaf9858fcfafc0750ba654bcc7e42765a9ef82d
Gijs Siemer <gijs.siemer@me.com>
2023-12-15 11:51:12 +0100

    Updated documentation

--- Documentation/theme.config.tsx
+++ Documentation/theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Chilibot 🌶️</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/GijsSi/Chilibot',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/GijsSi/Chilibot',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Documentation by Gijs Siemer',
   },
 }
 
